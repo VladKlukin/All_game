ac702abfda39c4cca71397f25a6ae96783d64afd
vlad <Klu90645@gmail.com>
2025-11-26 10:52:48 +0500

    иницилизация поля

--- game.cpp
+++ game.cpp
@@ -15,3 +15,12 @@ Game::Game(int size, std::string gameMode) : gridSize(size), score(0), mode(game
     }
     initializeGrid();
 }
+void Game::initializeGrid() {//иницилизация поля
+    grid.resize(gridSize, std::vector<Tile>(gridSize));
+    for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridSize; j++) {
+            grid[i][j] = Tile(rand() % colors.size());
+        }
+    }
+    ensureNoMatches();
+}
