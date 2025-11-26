6fa13fb61ac37464d1bce0889480fb950b21138f
vlad <Klu90645@gmail.com>
2025-11-26 10:18:05 +0500

    функция для изменения ходов и времени

---
+++ game.cpp
@@ -0,0 +1,17 @@
+#include "game.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <utility>
+
+Game::Game(int size, std::string gameMode) : gridSize(size), score(0), mode(gameMode) {
+    if (mode == "moves") {
+        movesLeft = 20;
+        timeLeft = -1;
+    } else {
+        movesLeft = -1;
+        timeLeft = 60;
+    }
+    initializeGrid();
+}
