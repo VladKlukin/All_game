87589ef8f016141a4e62e508ce518034342a9254
vlad <Klu90645@gmail.com>
2025-11-27 14:04:49 +0500

    Добавление функции иницилизации поля

--- game.cpp
+++ game.cpp
@@ -17,3 +17,12 @@ Game::Game(int size, string gameMode) : gridSize(size), score(0), mode(gameMode)
     }
     initializeGrid();
 }
+void Game::initializeGrid() {
+    grid.resize(gridSize, vector<Tile>(gridSize));
+    for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridSize; j++) {
+            grid[i][j] = Tile(rand() % colors.size());
+        }
+    }
+    ensureNoMatches();
+}
