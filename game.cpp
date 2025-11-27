1f2e4a67acfb811bdb01ee6a889166046be6281b
vlad <Klu90645@gmail.com>
2025-11-27 14:11:18 +0500

    Добавление функции, которая ищет совпадения(три в ряд)

--- game.cpp
+++ game.cpp
@@ -35,3 +35,33 @@ void Game::ensureNoMatches() {
         }
     }
 }
+vector<pair<int, int>> Game::findMatches() {
+    vector<pair<int, int>> matches;
+
+    for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridSize - 2; j++) {
+            if (grid[i][j].color == grid[i][j + 1].color &&
+                grid[i][j].color == grid[i][j + 2].color) {
+                matches.push_back({ i, j });
+                matches.push_back({ i, j + 1 });
+                matches.push_back({ i, j + 2 });
+            }
+        }
+    }
+
+    for (int i = 0; i < gridSize - 2; i++) {
+        for (int j = 0; j < gridSize; j++) {
+            if (grid[i][j].color == grid[i + 1][j].color &&
+                grid[i][j].color == grid[i + 2][j].color) {
+                matches.push_back({ i, j });
+                matches.push_back({ i + 1, j });
+                matches.push_back({ i + 2, j });
+            }
+        }
+    }
+
+    sort(matches.begin(), matches.end());
+    matches.erase(unique(matches.begin(), matches.end()), matches.end());
+
+    return matches;
+}
