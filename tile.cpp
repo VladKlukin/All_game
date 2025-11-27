dc347fe9989d056f78ab1c561c316d1d390f1b91
lena218 <krosovochka@gmail.com>
2025-11-27 14:58:16 +0500

    Добавила объявление приватных переменных

---
+++ tile.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <string>
+#include "tile.h"
+using namespace std;
+
+class Game {
+private:
+    vector<vector<Tile>> grid;
+    int gridSize;
+    int score;
+    int movesLeft;
+    int timeLeft;
+    string mode;
+
+    const vector<string> colors = { "$", "&", "*", "#", "@" };
